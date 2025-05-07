b482fce7473882ce7d26c0b7aec6e5f3ed4da90a
JChris1ie <168684291+JChris1ie@users.noreply.github.com>
2025-05-07 14:05:36 -0400

    Touch ups

--- playlist.py
+++ playlist.py
@@ -1,35 +1,43 @@
 import song
 import csv
 import os
+import random
 
 class Playlist:
+
     def __init__(self):
+        '''Initializes the playlist'''
         self.head=None
         self.tail=None
 
         self.currentSong=None # Advanced feature variable
 
+        noDelete=['recommend.csv'] # Extra feature variable
+
     def isEmpty(self):
+        '''Returns if list has values or not'''
         return self.head is None
     
     def addSongAtEnd(self,title,artist,duration):
+        '''Adds a new song node to the end of current linked list'''
         newSong=song.Song(title,artist,duration)
 
-        if self.head is None:
+        if self.isEmpty():
             self.head=newSong
             return
         
         current=self.head
-        while current.next:
+        while current.next: # Traverse to end and reassign tail and pointers
             current=current.next
         current.next=newSong
         newSong.prev=current
         self.tail=newSong
 
     def addSongAtBeginning(self,title,artist,duration):
+        '''Adds new song node to beginning of linked list'''
         newSong=song.Song(title,artist,duration)
 
-        if self.head is None:
+        if self.isEmpty():
             self.head=newSong
             return
         elif self.head is not None and self.tail is None:
@@ -46,32 +54,56 @@ class Playlist:
         nextSong.prev=self.head
 
     def removeSongByTitle(self,title):
-        if self.head is None:
+        '''Searches for specific song by title and removes it from the linked list'''
+        if self.isEmpty():
             print("No songs in playlist")
             return
         
         current=self.head
         while current:
-            if current.title==title:
+            if current.title==title and current.prev and current.next: # Removing middle node
                 prev=current.prev
                 next=current.next
                 prev.next=next
                 next.prev=prev
                 print('Song removed')
+                return
+            elif current.title==title and current.prev and not current.next: # No next node
+                self.removeLastSong()
+                return
+            elif current.title==title and not current.prev and current.next: # No prev node
+                self.head=self.head.next
+                self.head.prev=None
+                print('Song removed')
+                return
+            elif self.head==self.tail: # head is same as tail
+                self.removeLastSong()
+                return
             current=current.next
         print(f'Song "{title}" not found')
 
     def removeLastSong(self):
+        '''Reassigns and modifies tail pointer to remove last value from linked list'''
         if self.tail is None and self.head is None:
             print("Playlist is empty")
+            return
         elif self.tail is None and self.head is not None:
             self.head=None
+            print('Song removed')
+            return
+        elif self.head==self.tail:
+            self.head=None
+            self.tail=None
+            print('Song removed')
+            return
                     
         self.tail=self.tail.prev
         self.tail.next=None
+        print('Song removed')
 
     def display_playlist(self):
-        if self.head is None:
+        '''Prints details for each song node'''
+        if self.isEmpty():
             print('Playlist is empty')
             return
         
@@ -82,8 +114,9 @@ class Playlist:
             current=current.next
 
     def countSongs(self):
+        '''Iterates through linked list and counts number of nodes'''
         counter=0
-        if self.head is None:
+        if self.isEmpty():
             print(f'Number of songs: {counter}')
             return
         
@@ -94,16 +127,18 @@ class Playlist:
 
         print(f'Number of songs: {counter}')
 
-    def findSong(self,title): # main should handle output
+    def findSong(self,title):
+        '''Searches through linked list for specific title'''
         current=self.head
         while current:
             if current.title==title:
-                return True
+                return current
             current=current.next
         return False
 
     def reversePlaylist(self):
-        if self.head is None:
+        '''Reverses playlist in place'''
+        if self.isEmpty():
             print('Playlist is empty')
             return
         
@@ -119,16 +154,27 @@ class Playlist:
         tail=self.tail
         self.tail=self.head
         self.head=tail
+        print('playlist reversed')
 
     '''------------------------ Advanced Features ------------------------'''
 
     ''' Play Mode '''
     def playCurrent(self):
+        '''Prints details for current song playing'''
+        if self.isEmpty():
+            print('Playlist is empty')
+            return
+
         if self.currentSong is None:
             self.currentSong=self.head
         print(self.currentSong)
     
     def nextTrack(self):
+        '''Advances current track and wraps around when at end of playlist'''
+        if self.isEmpty():
+            print('Playlist is empty')
+            return
+        
         if self.currentSong is None:
             self.currentSong=self.head
         
@@ -138,6 +184,11 @@ class Playlist:
             self.currentSong=self.currentSong.next
     
     def previousTrack(self):
+        '''decrements track and wraps around when at the beginning'''
+        if self.isEmpty():
+            print('Playlist is empty')
+            return
+        
         if self.currentSong is None:
             self.currentSong=self.head
 
@@ -149,6 +200,11 @@ class Playlist:
     ''' Playlist Saving '''
 
     def saveToFile(self,filename):
+        '''Writes all playlist songs data to a csv file'''
+        if self.isEmpty():
+            print('Playlist is empty')
+            return
+        
         filename=filename+'.csv'
         with open(filename, mode='w',newline='',encoding='utf-8') as file:
             writer = csv.writer(file)
@@ -159,25 +215,38 @@ class Playlist:
                 current=current.next
 
     def loadFromFile(self,filename):
+        '''Reads all playlist song data from file'''
         filename=filename+'.csv'
-        with open(filename,mode='r',encoding='utf-8') as file:
-            reader=csv.DictReader(file)
-            for row in reader:
-                self.addSongAtEnd(row['Title'],row['Artist'],int(row['Duration']))
+        self.clearCurrentPlaylist()
+        try:
+            with open(filename,mode='r',encoding='utf-8') as file:
+                reader=csv.DictReader(file)
+                for row in reader:
+                    self.addSongAtEnd(row['Title'],row['Artist'],int(row['Duration']))
+            print('Playlist loaded')
+        except:
+            print(f'File {filename[:-4]} not found')
 
     ''' Extra for playlist file management'''
 
     def deletePlaylist(self,filename):
+        '''Erases playlist csv file from storage'''
         filename=filename+'.csv'
         try:
             os.remove(filename)
-            print('Playlist deleted')
+            print(f'Playlist {filename[:-4]} deleted')
         except:
-            print("Playlist does not exist")
+            print(f"Playlist {filename[:-4]} does not exist")
 
     ''' Extra for testing and QOL'''
     
     def clearCurrentPlaylist(self):
+        '''Empties playlist of song data'''
+        if self.isEmpty():
+            print('Playlist is empty, no songs cleared')
+            return
+        
         self.head=None
         self.tail=None
         self.currentSong=None
+        print('Cleared playlist')
